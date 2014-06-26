65e023c0cbd1cf6004165b474d28ae9aed31e37f
Johnathan Weisner <joweisner@gmail.com>
2014-06-26 14:47:02 -0400

    Gathering link_ids with RedditParser works, next step build a test to prove it.

---
+++ lib/reddit_parser.rb
@@ -0,0 +1,16 @@
+require 'snoo'
+
+class RedditParser
+  attr_accessor :subreddit
+  def initialize(args)
+    @reddit = args.fetch(:reddit)
+    @subreddit = args.fetch(:subreddit)
+  end
+
+  def link_ids
+    (@reddit.get_listing subreddit: subreddit)["data"]["children"]
+      .map{ |link| link["data"]["id"] }
+  end
+
+end
+
