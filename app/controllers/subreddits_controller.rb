0b6bfef3df6a64d3e2a092906d00fd33a5bf8f06
Johnathan Weisner <joweisner@gmail.com>
2014-07-22 14:27:19 -0400

    Added count total graph

--- app/controllers/subreddits_controller.rb
+++ app/controllers/subreddits_controller.rb
@@ -3,11 +3,12 @@ class SubredditsController < ApplicationController
     @todays_graph_data = Comment.joins(:subreddit).group(:name).where( "commented_on >= ?", 
       Time.zone.now.beginning_of_day).where("reply_status != 'never'").count
     subreddits = Subreddit.where(name: @todays_graph_data.keys)
-
+    @todays_percentage_graph_data = Hash.new
+    
     subreddits.each do |subreddit|
       total_today = subreddit.comments.where( "commented_on >= ?", 
         Time.zone.now.beginning_of_day).count
-      @todays_graph_data[subreddit.name] = (@todays_graph_data[subreddit.name] / total_today.to_f) * 100
+      @todays_percentage_graph_data[subreddit.name] = (@todays_graph_data[subreddit.name] / total_today.to_f) * 100
     end
   end
 end
