5e39636edfdf90635eb7efcdcbaa3c4186acb92d
Johnathan Weisner <joweisner@gmail.com>
2014-07-22 14:02:32 -0400

    Order of operations fix

--- app/controllers/subreddits_controller.rb
+++ app/controllers/subreddits_controller.rb
@@ -7,7 +7,7 @@ class SubredditsController < ApplicationController
     subreddits.each do |subreddit|
       total_today = subreddit.comments.where( "commented_on >= ?", 
         Time.zone.now.beginning_of_day).count
-      @todays_graph_data[subreddit.name] = @todays_graph_data[subreddit.name] / total_today * 100
+      @todays_graph_data[subreddit.name] = (@todays_graph_data[subreddit.name] / total_today) * 100
     end
   end
 end
