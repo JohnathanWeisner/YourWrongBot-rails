616a236be1e31e7d71233715d58b55c905f034d5
Johnathan Weisner <joweisner@gmail.com>
2014-07-22 13:33:29 -0400

    Moved graph data into the controller

--- app/controllers/subreddits_controller.rb
+++ app/controllers/subreddits_controller.rb
@@ -1,4 +1,6 @@
 class SubredditsController < ApplicationController
   def index
+    @todays_graph_data = Comment.joins(:subreddit).group(:name).where( "commented_on >= ?", 
+      Time.zone.now.beginning_of_day).where("reply_status != 'never'").count
   end
 end
