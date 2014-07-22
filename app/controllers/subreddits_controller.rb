7c77b85102f93889d485418016e7da36a9339f9b
Johnathan Weisner <joweisner@gmail.com>
2014-07-22 13:58:37 -0400

    Changed the number wrong into a percentage and labeled the axises

--- app/controllers/subreddits_controller.rb
+++ app/controllers/subreddits_controller.rb
@@ -2,5 +2,12 @@ class SubredditsController < ApplicationController
   def index
     @todays_graph_data = Comment.joins(:subreddit).group(:name).where( "commented_on >= ?", 
       Time.zone.now.beginning_of_day).where("reply_status != 'never'").count
+    subreddits = Subreddit.where(name: @todays_graph_data.keys)
+
+    subreddits.each do |subreddit|
+      total_today = subreddit.comments.where( "commented_on >= ?", 
+        Time.zone.now.beginning_of_day).count
+      @todays_graph_data[subreddit.name] = @todays_graph_data[subreddit.name] / total_today * 100
+    end
   end
 end
